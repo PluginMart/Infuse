fcf7311a27c942019e8cdaba62c45e8dea24a7b1
TurboJax <turbojax07@gmail.com>
2026-09-20 12:19:18 -0400

    Making offhand ability activation run the command

--- src/main/java/com/catadmirer/infuseSMP/listeners/PlayerSwapHandItemsListener.java
+++ src/main/java/com/catadmirer/infuseSMP/listeners/PlayerSwapHandItemsListener.java
@@ -1,10 +1,6 @@
 package com.catadmirer.infuseSMP.listeners;
 
-import com.catadmirer.infuseSMP.effects.InfuseEffect;
-import com.catadmirer.infuseSMP.managers.CooldownManager;
 import com.catadmirer.infuseSMP.managers.DataManager;
-
-import java.util.UUID;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -19,30 +15,19 @@ public class PlayerSwapHandItemsListener implements Listener {
 
     /**
      * Listens for when the player swaps the items in their main and offhand.
-     * When they do so, it will be used to activate their left or right spark based on whether or not they are crouching.
+     * When they do so, it will be used to activate their left or right spark based on whether they are crouching.
      *
      * @param event The {@link PlayerSwapHandItemsEvent} to process
      */
     @EventHandler
     public void onPlayerSwapHandItems(PlayerSwapHandItemsEvent event) {
         Player player = event.getPlayer();
-        UUID playerUUID = player.getUniqueId();
-        if (!dataManager.getControlMode(playerUUID).equals("offhand")) return;
+        if (!dataManager.getControlMode(player.getUniqueId()).equals("offhand")) return;
 
         if (!player.isSneaking()) {
-            // Activating the left effect's spark if the player was not sneaking and the effect wasn't on cooldown.
-            InfuseEffect lEffect = dataManager.getEffect(player.getUniqueId(), "1");
-            if (lEffect == null) return;
-            if (CooldownManager.isOnCooldown(playerUUID, lEffect.getPlainKey())) return;
-            event.setCancelled(true);
-            lEffect.activateSpark(player, "1");
+            player.performCommand("/lspark");
         } else {
-            // Activating the right effect's spark if the player was sneaking and the effect wasn't on cooldown.
-            InfuseEffect rEffect = dataManager.getEffect(player.getUniqueId(), "2");
-            if (rEffect == null) return;
-            if (CooldownManager.isOnCooldown(playerUUID, rEffect.getPlainKey())) return;
-            event.setCancelled(true);
-            rEffect.activateSpark(player, "2");
+            player.performCommand("/rspark");
         }
     }
 }
